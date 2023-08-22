a2bd1f3711eeffd889409f98aff941833d48d81c
Raymark Peña <dev.mlimp@gmail.com>
2023-08-22 18:30:11 +0800

    added portal routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,6 +35,11 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware('web')
+                ->prefix('portal')
+                ->as('portal.')
+                ->group(base_path('routes/portal.php'));
         });
     }
 }
